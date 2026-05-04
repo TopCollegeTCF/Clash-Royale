2610dec46326233cdf29d54027982acdc2e8779c
t1mch1k12 <kim228455@gmail.com>
2026-05-04 17:24:52 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,10 @@
-
+// Параметры игрока
+            const PLAYER_SIZE = 40;      // сторона квадрата (px)
+            const PLAYER_SPEED = 5;      // пикселей за кадр (плавное движение)
+            
+            // Начальная позиция (по центру)
+            let player = {
+                x: canvas.width/2 - PLAYER_SIZE/2,
+                y: canvas.height/2 - PLAYER_SIZE/2,
+                width: PLAYER_SIZE,
+                height: PLAYER_SIZE
